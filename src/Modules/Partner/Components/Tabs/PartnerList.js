2143755ee9e7b4c963278a545ff7ca8cb90ce961
Nguyễn Tuấn Vũ <nguyentuanvu231198@gmail.com>
2021-09-19 12:13:02 +0700

    partner list

---
+++ src/Modules/Partner/Components/Tabs/PartnerList.js
@@ -0,0 +1,422 @@
+import React from 'react';
+
+const ParnerList = () => {
+  return (
+    <section className="partner-area">
+      <div className="row">
+        <div className="col-lg-3 col-md-6">
+          <div className="partner-group">
+            <div className="partner-info-visible">
+              <div className="partner-info-box">
+                <div className="partner-info">
+                  <div className="partner-info-name">
+                    <span>Cô Phan Thị Ngọc Hân</span>
+                  </div>
+                  <div className="partner-special">
+                    <div className="partner-special-title">
+                      <span>Thành tích nổi bậc</span>
+                    </div>
+                    <div className="partner-special-list">
+                      <ul>
+                        <li>
+                          Bằng khen Thành Phố hoàn thành xuất sắc nhiệm vụ năm
+                          học.
+                        </li>
+                        <li>Giáo viên giỏi cấp Quận nhiều năm liền.</li>
+                      </ul>
+                    </div>
+                    <div className="partner-special-devider"></div>
+                    <div className="partner-special-action">
+                      <a className="btn-action btn-white">
+                        <span> Kinh nghiệm giảng dạy: 20 năm</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="partner-group-image">
+              <img
+                src={
+                  require('@Modules/Introduce/assets/images/partner-img1.png')
+                    .default
+                }
+                alt="partner-img"
+              />
+            </div>
+
+            <div className="partner-group-name">
+              <span>Thầy Bùi Văn Ngợi</span>
+            </div>
+            <div className="partner-group-introduce">
+              <ul>
+                <li>Người đầu tiên Việt Nam chinh phục đỉnh EVEREST</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-3 col-md-6">
+          <div className="partner-group">
+            <div className="partner-info-visible">
+              <div className="partner-info-box">
+                <div className="partner-info">
+                  <div className="partner-info-name">
+                    <span>Cô Phan Thị Ngọc Hân</span>
+                  </div>
+                  <div className="partner-special">
+                    <div className="partner-special-title">
+                      <span>Thành tích nổi bậc</span>
+                    </div>
+                    <div className="partner-special-list">
+                      <ul>
+                        <li>
+                          Bằng khen Thành Phố hoàn thành xuất sắc nhiệm vụ năm
+                          học.
+                        </li>
+                        <li>Giáo viên giỏi cấp Quận nhiều năm liền.</li>
+                      </ul>
+                    </div>
+                    <div className="partner-special-devider"></div>
+                    <div className="partner-special-action">
+                      <a className="btn-action btn-white">
+                        <span> Kinh nghiệm giảng dạy: 20 năm</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="partner-group-image">
+              <img
+                src={
+                  require('@Modules/Introduce/assets/images/partner-img2.png')
+                    .default
+                }
+                alt="partner-img"
+              />
+            </div>
+
+            <div className="partner-group-name">
+              <span>Thầy Phạm Ngọc Bình</span>
+            </div>
+            <div className="partner-group-introduce">
+              <ul>
+                <li>Kỷ lục gia Siêu Trí Tuệ Tâm Trí Lực</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-3 col-md-6">
+          <div className="partner-group">
+            <div className="partner-info-visible">
+              <div className="partner-info-box">
+                <div className="partner-info">
+                  <div className="partner-info-name">
+                    <span>Cô Phan Thị Ngọc Hân</span>
+                  </div>
+                  <div className="partner-special">
+                    <div className="partner-special-title">
+                      <span>Thành tích nổi bậc</span>
+                    </div>
+                    <div className="partner-special-list">
+                      <ul>
+                        <li>
+                          Bằng khen Thành Phố hoàn thành xuất sắc nhiệm vụ năm
+                          học.
+                        </li>
+                        <li>Giáo viên giỏi cấp Quận nhiều năm liền.</li>
+                      </ul>
+                    </div>
+                    <div className="partner-special-devider"></div>
+                    <div className="partner-special-action">
+                      <a className="btn-action btn-white">
+                        <span> Kinh nghiệm giảng dạy: 20 năm</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="partner-group-image">
+              <img
+                src={
+                  require('@Modules/Introduce/assets/images/partner-img3.png')
+                    .default
+                }
+                alt="partner-img"
+              />
+            </div>
+
+            <div className="partner-group-name">
+              <span>Cô Võ Thị Thái Vượng</span>
+            </div>
+            <div className="partner-group-introduce">
+              <ul>
+                <li>Giám đốc phòng đào tạo Tâm Trí Lực</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="partner-group">
+            <div className="partner-info-visible">
+              <div className="partner-info-box">
+                <div className="partner-info">
+                  <div className="partner-info-name">
+                    <span>Cô Phan Thị Ngọc Hân</span>
+                  </div>
+                  <div className="partner-special">
+                    <div className="partner-special-title">
+                      <span>Thành tích nổi bậc</span>
+                    </div>
+                    <div className="partner-special-list">
+                      <ul>
+                        <li>
+                          Bằng khen Thành Phố hoàn thành xuất sắc nhiệm vụ năm
+                          học.
+                        </li>
+                        <li>Giáo viên giỏi cấp Quận nhiều năm liền.</li>
+                      </ul>
+                    </div>
+                    <div className="partner-special-devider"></div>
+                    <div className="partner-special-action">
+                      <a className="btn-action btn-white">
+                        <span> Kinh nghiệm giảng dạy: 20 năm</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="partner-group-image">
+              <img
+                src={
+                  require('@Modules/Introduce/assets/images/teacher-img4.png')
+                    .default
+                }
+                alt="partner-img"
+              />
+            </div>
+
+            <div className="partner-group-name">
+              <span>Cô Hương Xuân</span>
+            </div>
+            <div className="partner-group-introduce">
+              <ul>
+                <li>Chuyên viên đào tạo</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="partner-group">
+            <div className="partner-info-visible">
+              <div className="partner-info-box">
+                <div className="partner-info">
+                  <div className="partner-info-name">
+                    <span>Cô Phan Thị Ngọc Hân</span>
+                  </div>
+                  <div className="partner-special">
+                    <div className="partner-special-title">
+                      <span>Thành tích nổi bậc</span>
+                    </div>
+                    <div className="partner-special-list">
+                      <ul>
+                        <li>
+                          Bằng khen Thành Phố hoàn thành xuất sắc nhiệm vụ năm
+                          học.
+                        </li>
+                        <li>Giáo viên giỏi cấp Quận nhiều năm liền.</li>
+                      </ul>
+                    </div>
+                    <div className="partner-special-devider"></div>
+                    <div className="partner-special-action">
+                      <a className="btn-action btn-white">
+                        <span> Kinh nghiệm giảng dạy: 20 năm</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="partner-group-image">
+              <img
+                src={
+                  require('@Modules/Introduce/assets/images/teacher-img5.png')
+                    .default
+                }
+                alt="partner-img"
+              />
+            </div>
+
+            <div className="partner-group-name">
+              <span>Thầy Bùi Văn Ngợi</span>
+            </div>
+            <div className="partner-group-introduce">
+              <ul>
+                <li>Người đầu tiên Việt Nam chinh phục đỉnh EVEREST</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="partner-group">
+            <div className="partner-info-visible">
+              <div className="partner-info-box">
+                <div className="partner-info">
+                  <div className="partner-info-name">
+                    <span>Cô Phan Thị Ngọc Hân</span>
+                  </div>
+                  <div className="partner-special">
+                    <div className="partner-special-title">
+                      <span>Thành tích nổi bậc</span>
+                    </div>
+                    <div className="partner-special-list">
+                      <ul>
+                        <li>
+                          Bằng khen Thành Phố hoàn thành xuất sắc nhiệm vụ năm
+                          học.
+                        </li>
+                        <li>Giáo viên giỏi cấp Quận nhiều năm liền.</li>
+                      </ul>
+                    </div>
+                    <div className="partner-special-devider"></div>
+                    <div className="partner-special-action">
+                      <a className="btn-action btn-white">
+                        <span> Kinh nghiệm giảng dạy: 20 năm</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="partner-group-image">
+              <img
+                src={
+                  require('@Modules/Introduce/assets/images/teacher-img6.png')
+                    .default
+                }
+                alt="partner-img"
+              />
+            </div>
+
+            <div className="partner-group-name">
+              <span>Thầy Phạm Ngọc Bình</span>
+            </div>
+            <div className="partner-group-introduce">
+              <ul>
+                <li>Kỷ lục gia Siêu Trí Tuệ Tâm Trí Lực</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="partner-group">
+            <div className="partner-info-visible">
+              <div className="partner-info-box">
+                <div className="partner-info">
+                  <div className="partner-info-name">
+                    <span>Cô Phan Thị Ngọc Hân</span>
+                  </div>
+                  <div className="partner-special">
+                    <div className="partner-special-title">
+                      <span>Thành tích nổi bậc</span>
+                    </div>
+                    <div className="partner-special-list">
+                      <ul>
+                        <li>
+                          Bằng khen Thành Phố hoàn thành xuất sắc nhiệm vụ năm
+                          học.
+                        </li>
+                        <li>Giáo viên giỏi cấp Quận nhiều năm liền.</li>
+                      </ul>
+                    </div>
+                    <div className="partner-special-devider"></div>
+                    <div className="partner-special-action">
+                      <a className="btn-action btn-white">
+                        <span> Kinh nghiệm giảng dạy: 20 năm</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="partner-group-image">
+              <img
+                src={
+                  require('@Modules/Introduce/assets/images/teacher-img7.png')
+                    .default
+                }
+                alt="partner-img"
+              />
+            </div>
+
+            <div className="partner-group-name">
+              <span>Cô Võ Thị Thái Vượng</span>
+            </div>
+            <div className="partner-group-introduce">
+              <ul>
+                <li>Giám đốc phòng đào tạo Tâm Trí Lực</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6">
+          <div className="partner-group">
+            <div className="partner-info-visible">
+              <div className="partner-info-box">
+                <div className="partner-info">
+                  <div className="partner-info-name">
+                    <span>Cô Phan Thị Ngọc Hân</span>
+                  </div>
+                  <div className="partner-special">
+                    <div className="partner-special-title">
+                      <span>Thành tích nổi bậc</span>
+                    </div>
+                    <div className="partner-special-list">
+                      <ul>
+                        <li>
+                          Bằng khen Thành Phố hoàn thành xuất sắc nhiệm vụ năm
+                          học.
+                        </li>
+                        <li>Giáo viên giỏi cấp Quận nhiều năm liền.</li>
+                      </ul>
+                    </div>
+                    <div className="partner-special-devider"></div>
+                    <div className="partner-special-action">
+                      <a className="btn-action btn-white">
+                        <span> Kinh nghiệm giảng dạy: 20 năm</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="partner-group-image">
+              <img
+                src={
+                  require('@Modules/Introduce/assets/images/teacher-img8.png')
+                    .default
+                }
+                alt="partner-img"
+              />
+            </div>
+
+            <div className="partner-group-name">
+              <span>Cô Hương Xuân</span>
+            </div>
+            <div className="partner-group-introduce">
+              <ul>
+                <li>Chuyên viên đào tạo</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ParnerList;
