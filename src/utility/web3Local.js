da53718553dd07fe6a48d13f871bfad7b303be1e
etkgjt <canlong102@gmail.com>
2021-12-03 21:12:03 +0700

    update

--- src/utility/web3Local.js
+++ src/utility/web3Local.js
@@ -2,8 +2,6 @@ import Web3 from "web3"
 let web3 = null
 if (window?.ethereum) {
   window.ethereum.enable()
-} else {
-  web3 = new Web3(window?.web3?.currentProvider)
 }
-
+web3 = new Web3(window?.web3?.currentProvider)
 export default web3
